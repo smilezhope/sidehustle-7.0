8c0f04bf4244a8ca0a8105f19875d11f5fa5618c
smilezhope <chisomhope03@gmail.com>
2023-03-27 02:09:36 +0100

    new header file added

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-<?php
-$chisom = "<b>I am chisom and this is my week one task</b>";
-echo $chisom;
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <p> welcome to task 3 </p>
+    <?php
+    $connect = mysqli_connect("localhost", "root", "", "function_db");
+    if ($connect){
+        echo "connected";
+    }
+    ?>
+
+</body>
+</html>
